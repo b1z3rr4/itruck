c706516d484417310a374901924c8949836290ad
“b1z3rr4” <natalia@b1z3rr4.com.br>
2024-02-14 01:52:55 -0300

    persistindo os dados na storage

--- src/stores/cartStore.ts
+++ src/stores/cartStore.ts
@@ -1,6 +1,8 @@
 import { ProductProps } from "@/utils/data/products";
 import { create } from "zustand";
 import * as cartInMemory from './helpers/cartInMemory';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
 export type ProductCardProps = ProductProps & {
     quantity: number;
@@ -12,14 +14,21 @@ type StateProps = {
     remove: (product: ProductCardProps) => void;
 }
 
-export const useCartStore = create<StateProps>((set) => ({
-    products: [],
+export const useCartStore = create(
+    persist<StateProps>(
+        (set) => ({
+            products: [],
 
-    add: (product: ProductProps) => set((state) => ({
-        products: cartInMemory.add(state.products, product),
-    })),
-
-    remove: (product: ProductCardProps) => set((state) => ({
-        products: cartInMemory.remove(state.products, product),
-    })),
-}));
+            add: (product: ProductProps) => set((state) => ({
+                products: cartInMemory.add(state.products, product),
+            })),
+        
+            remove: (product: ProductCardProps) => set((state) => ({
+                products: cartInMemory.remove(state.products, product),
+            })),
+        }), {
+            name: 'itruck-cart',
+            storage: createJSONStorage(() =>  AsyncStorage),
+        }
+    )
+)
