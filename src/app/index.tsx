2dee874d6781eb740970aca87fcd4e504cb814c4
“b1z3rr4” <natalia@b1z3rr4.com.br>
2024-02-13 23:47:18 -0300

    home e header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,34 @@
-import {View, Text} from 'react-native';
+import { Category } from '@/components/category';
+import { Header } from '@/components/header';
+import { View, FlatList } from 'react-native';
+import { CATEGORIES } from "@/utils/data/products";
+import { useState } from 'react';
 
 export default function Home() {
+    const [categorySelected, setCatergorySelected] = useState(CATEGORIES[0]);
+
+    const handleCategorySelected = (category: string) => {
+        setCatergorySelected(category);
+    }
+
     return (
-        <View>
-            <Text>Hello!</Text>
+        <View className="flex-1 pt-8">
+            <Header title="Faça seu pedido" itemsQuantity={2} />
+            <FlatList 
+                data={CATEGORIES}
+                keyExtractor={(item) => item}
+                renderItem={({ item }) => (
+                    <Category 
+                        title={item} 
+                        isSelected={categorySelected === item} 
+                        onPress={() => handleCategorySelected(item)}
+                    />
+                )}
+                horizontal
+                className='max-h-10 mt-5'
+                contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+                showsHorizontalScrollIndicator={false}
+            />
         </View>
     );
 }
